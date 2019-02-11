84f9bd551281d758b166a8af5198df942b5bc57b
Rushi Gandhi <rushi.gandhi@hotmail.com>
2019-02-11 00:23:13 -0500

    Character Switches from X to O and Vice Versa

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -9,13 +9,22 @@ export class BoardComponent implements OnInit {
 
   // Null fill an empty array
   squares = Array(9).fill(null);
+  currentPlayer  = "X";
 
 
   play(location){
     console.log(location);
 
-    // When a square is clicked, show an X
-    this.squares[location] = "X";
+    // When a square is clicked, show a character
+    if(!this.squares[location]){
+      this.squares[location] = this.currentPlayer;
+      if(this.currentPlayer == "X"){
+        this.currentPlayer = "O";
+      }
+      else{
+        this.currentPlayer = "X";
+      }
+    }
   }
 
   constructor() { }
