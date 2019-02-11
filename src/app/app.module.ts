f7617201aa64d57b4fffdbcefab8691774469014
Rushi Gandhi <rushi.gandhi@hotmail.com>
2019-02-11 01:06:55 -0500

    Added Styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { SquareComponent } from './components/square/square.component';
     SquareComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
